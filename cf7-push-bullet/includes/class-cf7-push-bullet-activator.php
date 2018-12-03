d22e8a62f6e12a563cc0cb2a44cf8d8bf0ceec48
Cristian Rat <kilroy05@gmail.com>
2018-12-03 18:12:50 +0000

    Remove comment and unnecessary function

--- cf7-push-bullet/includes/class-cf7-push-bullet-activator.php
+++ cf7-push-bullet/includes/class-cf7-push-bullet-activator.php
@@ -37,9 +37,6 @@ class Cf7_Push_Bullet_Activator
     {
         // prepare tables
         self::prepare_database_tables();
-
-        // and add some random dev data
-        self::add_testing_data();
     }
 
     /*
@@ -82,112 +79,6 @@ class Cf7_Push_Bullet_Activator
 //            dbDelta($sql);
 //            update_option("cf7_push_bullet_db_version", $cf7_push_bullet_db_version);
 //        }
-    }
-
-    /*
-     * Add testing data
-     *
-     * @since    1.0.0
-     */
-    public static function add_testing_data()
-    {
-        global $wpdb;
-
-        $table_name = $wpdb->prefix . self::$table_name;
-
-        $wpdb->insert(
-            $table_name,
-                array(
-                    'date' => current_time('mysql'),
-                    'push_type' => 'note',
-                    'push_title' => 'new form submission on www.....',
-                    'push_body' => 'form: asdadad
-                                    usename: asdasd
-                                    asdads: XX
-                                    emai: asdadafas@asdasd.com
-                                    phone: 9484987328782374',
-                    'push_reply' => '{
-                                    "active": true,
-                                    "iden": "ujDsTpyFe1YsjAlZ7UNk28",
-                                    "created": 1543417345.573496,
-                                    "modified": 1543417345.588333,
-                                    "type": "note",
-                                    "dismissed": false,
-                                    "direction": "self",
-                                    "sender_iden": "ujDsTpyFe1Y",
-                                    "sender_email": "kilroy05@gmail.com",
-                                    "sender_email_normalized": "kilroy05@gmail.com",
-                                    "sender_name": "Ra Chris",
-                                    "receiver_iden": "ujDsTpyFe1Y",
-                                    "receiver_email": "kilroy05@gmail.com",
-                                    "receiver_email_normalized": "kilroy05@gmail.com",
-                                    "title": "new form submission on www.....",
-                                    "body": "form: asdadad\nusename: asdasd\nasdads: XX\nemai: asdadafas@asdasd.com\nphone: 9484987328782374"
-                                }',
-                )
-        );
-        $wpdb->insert(
-            $table_name,
-            array(
-                'date' => current_time('mysql'),
-                'push_type' => 'note',
-                'push_title' => 'new form submission on www.....',
-                'push_body' => 'form: asdadad
-                                    usename: asdasd
-                                    asdads: XX
-                                    emai: asdadafas@asdasd.com
-                                    phone: 9484987328782374',
-                'push_reply' => '{
-                                    "active": true,
-                                    "iden": "ujDsTpyFe1YsjAlZ7UNk28",
-                                    "created": 1543417345.573496,
-                                    "modified": 1543417345.588333,
-                                    "type": "note",
-                                    "dismissed": false,
-                                    "direction": "self",
-                                    "sender_iden": "ujDsTpyFe1Y",
-                                    "sender_email": "kilroy05@gmail.com",
-                                    "sender_email_normalized": "kilroy05@gmail.com",
-                                    "sender_name": "Ra Chris",
-                                    "receiver_iden": "ujDsTpyFe1Y",
-                                    "receiver_email": "kilroy05@gmail.com",
-                                    "receiver_email_normalized": "kilroy05@gmail.com",
-                                    "title": "new form submission on www.....",
-                                    "body": "form: asdadad\nusename: asdasd\nasdads: XX\nemai: asdadafas@asdasd.com\nphone: 9484987328782374"
-                                }',
-            )
-        );
-        $wpdb->insert(
-            $table_name,
-            array(
-                'date' => current_time('mysql'),
-                'push_type' => 'note',
-                'push_title' => 'new form submission on www.....',
-                'push_body' => 'form: asdadad
-                                    usename: asdasd
-                                    asdads: XX
-                                    emai: asdadafas@asdasd.com
-                                    phone: 9484987328782374',
-                'push_reply' => '{
-                                    "active": true,
-                                    "iden": "ujDsTpyFe1YsjAlZ7UNk28",
-                                    "created": 1543417345.573496,
-                                    "modified": 1543417345.588333,
-                                    "type": "note",
-                                    "dismissed": false,
-                                    "direction": "self",
-                                    "sender_iden": "ujDsTpyFe1Y",
-                                    "sender_email": "kilroy05@gmail.com",
-                                    "sender_email_normalized": "kilroy05@gmail.com",
-                                    "sender_name": "Ra Chris",
-                                    "receiver_iden": "ujDsTpyFe1Y",
-                                    "receiver_email": "kilroy05@gmail.com",
-                                    "receiver_email_normalized": "kilroy05@gmail.com",
-                                    "title": "new form submission on www.....",
-                                    "body": "form: asdadad\nusename: asdasd\nasdads: XX\nemai: asdadafas@asdasd.com\nphone: 9484987328782374"
-                                }',
-            )
-        );
     }
 
 }
