6477c19b9246b16b84312b607803ef4cc7d5e2a2
Cristian Rat <kilroy05@gmail.com>
2018-12-04 12:14:41 +0000

    Add github link

--- cf7-push-bullet/admin/partials/tab-about.php
+++ cf7-push-bullet/admin/partials/tab-about.php
@@ -4,5 +4,5 @@
 <br />
     <h3>This plugin is free for use and developed by <a href="https://griffin.digital" target="_blank">https://Griffin.Digital</a></h3>
     <p>We welcome any questions, requests or comments. Please <a href="https://griffin.digital/contact/" target="_blank">contact us</a>.</p>
-    <p>The repository is hosted on GitHub. We welcome any pull requests.</p>
+    <p>The repository is hosted on <a href="https://github.com/griffindigitalsolutions" target="_blank">GitHub</a>. We welcome any pull requests.</p>
 </div>
