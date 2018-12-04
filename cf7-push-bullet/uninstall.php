e5677b4c07e2218933b6f804801b36d269838654
Cristian Rat <kilroy05@gmail.com>
2018-12-04 08:18:43 +0000

    Add uninstaller

--- cf7-push-bullet/uninstall.php
+++ cf7-push-bullet/uninstall.php
@@ -1,31 +1,20 @@
 <?php
 
-/**
- * Fired when the plugin is uninstalled.
- *
- * When populating this file, consider the following flow
- * of control:
- *
- * - This method should be static
- * - Check if the $_REQUEST content actually is the plugin name
- * - Run an admin referrer check to make sure it goes through authentication
- * - Verify the output of $_GET makes sense
- * - Repeat with other user roles. Best directly by using the links/query string parameters.
- * - Repeat things for multisite. Once for a single site in the network, once sitewide.
- *
- * This file may be updated more in future version of the Boilerplate; however, this is the
- * general skeleton and outline for how the file should work.
- *
- * For more information, see the following discussion:
- * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
- *
- * @link       https://griffin.digital
- * @since      1.0.0
- *
- * @package    Cf7_Push_Bullet
- */
+// On uninstalling plugin drop table
+// and remove the APIKEY from the options
 
 // If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+
+
+// remove option
+delete_option('cf7-push-bullet-settings-api-key');
+
+// drop table
+global $wpdb;
+$table_name = $wpdb->prefix . 'cf7_push_bullet';
+
+$query = $wpdb->prepare( 'DROP TABLE %S', $wpdb->esc_like( $table_name ) );
+
